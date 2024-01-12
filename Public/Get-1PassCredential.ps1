b53a5bc587ab41f3592dbcf273cf7bc51f86e5fa
Tore Melberg <tore@melberg.netgit>
2024-01-12 12:34:06 +0100

    added clipboard functionality

--- Public/Get-1PassCredential.ps1
+++ Public/Get-1PassCredential.ps1
@@ -2,15 +2,38 @@ Function Get-1PassCredential {
     [cmdletBinding()]
     param(
         [parameter(Mandatory="True")][string]$title,
-        [string]$vaultName
+        [string]$vaultName, 
+        [switch]$Clipboard
     )
 
     try {
         if ($vaultName) {
             $credObject = op item get $title --vault $vaultName | convertfrom-Json
+
+            $credential = New-Object -TypeName PSCredential `
+            -ArgumentList (($credObject.fields | where-object {$_.Type -like "String" -and $_.purpose -like "UserName"}).value), 
+                (($($credObject.fields | where-object {$_.Type -like "Concealed" -and $_.purpose -like "Password"}).value | convertTo-SecureString -asPlainText -Force))
+
+            return $credential
+        
+        if ($Clipboard) {
+            $credential.GetNetworkCredential().password | Set-Clipboard
+        }
+    
         }
         else {
             $credObject = op item get $title | ConvertFrom-Json
+
+            $credential = New-Object -TypeName PSCredential `
+            -ArgumentList (($credObject.fields | where-object {$_.Type -like "String" -and $_.purpose -like "UserName"}).value), 
+                (($($credObject.fields | where-object {$_.Type -like "Concealed" -and $_.purpose -like "Password"}).value | convertTo-SecureString -asPlainText -Force))
+
+            return $credential
+        
+        if ($Clipboard) {
+            $credential.GetNetworkCredential().password | Set-Clipboard
+        }
+    
         }
     }
     catch {
@@ -18,8 +41,4 @@ Function Get-1PassCredential {
     }
     
 
-    New-Object -TypeName PSCredential `
-        -ArgumentList (($credObject.fields | where-object {$_.Type -like "String" -and $_.purpose -like "UserName"}).value), 
-            (($($credObject.fields | where-object {$_.Type -like "Concealed" -and $_.purpose -like "Password"}).value | convertTo-SecureString -asPlainText -Force))
-    
 }
