5bd3c0b7c705f80ca2b8b839b583434e28960ba1
Tore Melberg <tore@melberg.netgit>
2024-01-22 13:01:26 +0100

    added default 4 props

--- Public/Get-1PassVaultSecret.ps1
+++ Public/Get-1PassVaultSecret.ps1
@@ -6,12 +6,23 @@ Function Get-1PassVaultSecret {
 
 
     # List credentials stored in specific vault
+    # create a PSPropertySet with the default property names
+  [string[]]$visible = 'Id','Title','Category, Tags'
+  [Management.Automation.PSMemberInfo[]]$visibleProperties = [System.Management.Automation.PSPropertySet]::new('DefaultDisplayPropertySet',$visible)
+   
+  # before outputting, append the PSStandardMembers property:
 
     if ($vaultName) {
         $credList = op item list --vault $vaultName | ConvertFrom-Json
+        $credList |
+        Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $visibleProperties -PassThru
+      
         return $credList
     }
     else {
+        $credList |
+        Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $visibleProperties -PassThru
+      
         $credList = op item list | convertfrom-Json
         return $credList
     }
