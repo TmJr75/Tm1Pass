928ca3c33d6c1bb9a9248d09ff7fae87ccbd54b6
Tore Melberg <tore@melberg.netgit>
2024-01-24 09:17:26 +0100

    changed default properties

--- Public/Get-1PassVaultSecret.ps1
+++ Public/Get-1PassVaultSecret.ps1
@@ -7,23 +7,27 @@ Function Get-1PassVaultSecret {
 
     # List credentials stored in specific vault
     # create a PSPropertySet with the default property names
-  [string[]]$visible = 'Id','Title','Category, Tags'
+  [string[]]$visible = 'Id','Title','Category','Tags'
   [Management.Automation.PSMemberInfo[]]$visibleProperties = [System.Management.Automation.PSPropertySet]::new('DefaultDisplayPropertySet',$visible)
    
   # before outputting, append the PSStandardMembers property:
 
     if ($vaultName) {
         $credList = op item list --vault $vaultName | ConvertFrom-Json
-        $credList |
+
+        $null = $credList |
         Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $visibleProperties -PassThru
       
         return $credList
     }
     else {
-        $credList |
-        Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $visibleProperties -PassThru
       
         $credList = op item list | convertfrom-Json
+
+        $null = $credList |
+        Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $visibleProperties -PassThru
+
+
         return $credList
     }
     
