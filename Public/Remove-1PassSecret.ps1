90849184b12a3652b3bd18789e19ba843ebd4707
Tore Melberg <tore@melberg.netgit>
2024-01-30 16:16:40 +0100

    added remove

---
+++ Public/Remove-1PassSecret.ps1
@@ -0,0 +1,32 @@
+<#
+.DESCRIPTION
+This function removes a credential from your vault based on the Title, Id or Sharelink properties.
+
+.PARAMETER Title
+This parameter can be either the Title of your secret, the Id of the secret, or the Sharelink of the secret.
+
+.PARAMETER VaultName
+Determines what vault the secret will be removed from.
+#>
+
+
+
+Function Remove-1PassSecret {
+    [CmdletBinding()]
+    Param(
+        [string][parameter(Mandatory=$true)]$VaultName,
+        [string][parameter(Mandatory=$true)]$Title,
+        [System.Management.Automation.SwitchParameter]$Archive
+
+    )
+
+
+    try {
+        Write-Output "Trying to remove secret: $Title Vault: $VaultName"
+        op item delete $Title --vault $VaultName --archive
+    }
+    catch {
+        Write-Output "Unable to remove secret: $Title  Vault: $VaultName"
+    }
+
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@
 The goal is to make it more "PowerShell'ish" in working with OnePass and its vaults and credentials.
 
 The wordlist used to generate PassPhrases are the wordlist from John Lawler, link to it can be found on this page: https://websites.umich.edu/~jlawler/wordlist.html
+
+# Functionality to be added
+[ ] Sign In
+[ ] Sign Out
+[ ] Check for CLI Update
+[ ] Information about signed in Account
+[ ] Delete Credential
+[ ] Users within this Account - Management
