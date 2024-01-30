7df106f2c78fef21efbaecc60d50b17b8551f159
Tore Melberg <tore@melberg.net>
2024-01-30 18:34:07 +0100

    updated remove-1passSecret

--- Public/Remove-1PassSecret.ps1
+++ Public/Remove-1PassSecret.ps1
@@ -1,3 +1,5 @@
+Function Remove-1PassSecret {
+
 <#
 .DESCRIPTION
 This function removes a credential from your vault based on the Title, Id or Sharelink properties.
@@ -8,10 +10,23 @@ Determines what vault the secret will be removed from.
 .SYNOPSIS
 Write more soon here
 #>
+<#
+.DESCRIPTION
+This function removes a credential from your vault based on the Title, Id or Sharelink properties.
+.PARAMETER Title
+This parameter can be either the Title of your secret, the Id of the secret, or the Sharelink of the secret.
+.PARAMETER VaultName
+Determines what vault the secret will be removed from.
+.SYNOPSIS
+Command to remove secrets from 1password vault.
+.EXAMPLE
+Remove-1PassSecret -Title xxxxx -VaultName xxxxxxx -Archive
+This removes the secret xxxxxx from your vault and puts it in the archive folder
+#>
 
 
 
-Function Remove-1PassSecret {
+
     [CmdletBinding()]
     Param(
         [string][parameter(Mandatory=$true)]$VaultName,
